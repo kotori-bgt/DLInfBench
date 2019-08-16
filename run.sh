e582c8f06cf766c356f780f5488b287c840e4899
kotori-bgt <45191617+kotori-bgt@users.noreply.github.com>
2019-08-16 15:19:51 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 PYTHON=python
-NETWORK_LIST="alexnet-v2 vgg16 vgg19 resnet101 resnet200 resnet_v2_50 resnet_v2_101 resnet_v2_200"
+NETWORK_LIST="alexnet alexnet-v2 vgg16 vgg19 resnet101 resnet200 resnet_v2_50 resnet_v2_101 resnet_v2_200"
 GPU=0
 DTYPE=float32
 BATCH_SIZE_LIST="2 4 8 16 32 64 128 256"
 N_EPOCH=3
 WARM_UP_NUM=3
-DLLIB_LIST="mxnet pytorch tensorflow"
+DLLIB_LIST="pytorch tensorflow"
 
 trap 'echo you hit Ctrl-C/Ctrl-\, now exiting..; pkill -P $$; exit' INT QUIT
 for DLLIB in ${DLLIB_LIST}
@@ -19,7 +19,7 @@ do
             CUDA_VISIBLE_DEVICES=${GPU} ${PYTHON} inference_${DLLIB}.py --network ${NETWORK} \
                 --dtype ${DTYPE} \
                 --batch-size ${BATCH_SIZE} \
-                --n-sample 5000 \
+                --n-sample 2000 \
                 --n-epoch ${N_EPOCH} \
                 --warm-up-num ${WARM_UP_NUM} \
                 --gpu 0
